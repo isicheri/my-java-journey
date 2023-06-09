92859f16345ef145ebe0ef6e661701f44094c23e
isicheri <tech2dom@gmail.com>
2023-06-10 00:59:58 +0100

    learning algorithms

---
+++ src/InputExample.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class InputExample {
+    enum Days { MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY}
+
+
+    public static void countNum(int[]  a, int v) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if(v == a[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void formatName(String firstName,String lastName) {
+        String a = firstName.substring(0,1);
+        String b = lastName.substring(0,1);
+
+        System.out.println(a.toUpperCase() +
+                firstName.substring(1,firstName.length()) + " " + b.toUpperCase() +
+                lastName.substring(1,lastName.length()));
+    }
+
+    public static void main(String[] args) throws IOException {
+//        Scanner scn = new Scanner(System.in);
+//        int num = scn.nextInt();
+//        int randNum = new Random().nextInt(10,11);
+//
+//        System.out.println(randNum);
+
+        Days day = Days.valueOf("MONDAY");
+        System.out.println(day);
+
+//        InputExample.countNum(new int[]{2,4,5,2,6,2,7,2},2);
+        InputExample.formatName("dominion","isicheri");
+    }
+}
